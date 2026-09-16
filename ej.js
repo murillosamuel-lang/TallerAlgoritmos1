1b7cb743160a30b42ead86446723a39150a2ce0e
Samuel Murillo <murillosamuel016@gmail.com>
2026-09-15 22:36:51 -0500

    Mi Primer Commit

---
+++ ej.js
@@ -0,0 +1,18 @@
+// nombre del equipo = SoftwareStyle
+
+/* Roles Piloto = Samuel Murillo
+         Navegante = Manuela Rodriguez
+         Cronista = Juan David
+         Auditor de IA = Hugo Marin
+*/
+
+let equipo = "softwareStyle";
+console.log(`Hola, somos ${equipo} y venimos a ganar`);
+
+let monedas = 120;
+
+if ( monedas >= 100) {
+    console.log("¡Adelante, viajero!");
+} else {
+    console.log(`Te faltan ${100 - monedas} monedas`);
+}
---
+++ nivel1relampago.js
@@ -0,0 +1,6 @@
+let clave = 0;
+if (clave) {
+    console.log("A");
+} else {
+    console.log("B");
+}
